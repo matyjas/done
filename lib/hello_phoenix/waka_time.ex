1f6f2585f5bf5b23a136711a9ec97d14c3b45402
Maciej Matyjas <matyjas@gmail.com>
2016-01-31 21:13:05 +0000

    nice little error message for when I forget to prepare that API key

--- lib/hello_phoenix/waka_time.ex
+++ lib/hello_phoenix/waka_time.ex
@@ -2,12 +2,12 @@ defmodule HelloPhoenix.WakaTime do
  use HTTPotion.Base
 
  def process_url(slug) do
-	 "https://wakatime.com/api/v1/" <> slug <> "?api_key=" <> System.get_env("WAKATIME_API_KEY")
+	 "https://wakatime.com/api/v1/" <> slug <> "?api_key=" <> wakatime_api_key
  end
 
  def process_response_body(body) do
-#	 IO.inspect(body)
 	 body
+#	 |> IO.inspect
 	 |> Poison.decode!
 	 |> Map.get("data")
 #	 |> Map.get("languages")
@@ -17,5 +17,12 @@ defmodule HelloPhoenix.WakaTime do
 	 |> elem(1)
 #	 |> Enum.map(fn(lang) -> Enum.filter(lang, fn({k,v}) -> (k == "name") end) end)
 #	 |> Enum.map(fn(l) -> Enum.find_value(l, fn({k,v}) -> if(k == "name") do v end end) end)
+ end
+
+ defp wakatime_api_key do
+	 case System.get_env("WAKATIME_API_KEY") do 
+		 nil -> raise("need that Wakatime API key")
+		 wakatime_api_key -> wakatime_api_key
+	 end
  end
 end
